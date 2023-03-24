8c409f950f6f680ff61799799999774ccaabe2e7
Robin Schatzl <tmai02476@gmail.com>
2023-03-24 11:12:12 +0100

    added Button with save funcion to Backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,43 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ActionBar from "./ActionBar";
-//import axios from "axios";
-
-function addToDoButton() {
-
-}
+import React, { useState } from 'react';
+import axios from 'axios';
+import ActionBar from './ActionBar';
 
 function App() {
+    const [addToDoText, setToDoText] = useState('');
 
-    const [addToDoText, setToDoText] = useState("")
     function onChange(value: string) {
-        setToDoText(value)
+        setToDoText(value);
     }
-  return (
 
-    <div className="App">
-      <header className="ToDo-List"></header>
-        <ActionBar inputText={addToDoText} onChange={onChange}/>
-        <p><button className="ButtonOne" onClick={addToDoButton}>
-            Click me </button></p>
-    </div>
-  );
+    function addToDoButton() {
+        const newToDo = addToDoText.trim();
+        if (newToDo !== '') {
+            axios
+                .post('/api/todo', {
+                    title: newToDo,
+                    done: false,
+                })
+                .then(function (response) {
+                    console.log(response);
+                    setToDoText('');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="ToDo-List"></header>
+            <ActionBar inputText={addToDoText} onChange={onChange} />
+            <p>
+                <button className="ButtonOne" onClick={addToDoButton}>
+                    Save ToDo
+                </button>
+            </p>
+        </div>
+    );
 }
 
 export default App;
